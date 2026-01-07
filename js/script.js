c4ef883424f4a04ece4b7dcf885489c5b725ed6a
pareesa969 <pareesakhan613@gmail.com>
2026-01-07 17:32:11 +0500

    feat: create const variables to getElementById

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const monthYear = document.getElementById('monthYear');
+const calendarDays = document.getElementById('calendarDays');
+const selectedDate = document.getElementById('selectedDate');
+
+const prevMonth = document.getElementById('prevMonth');
+const nextMonth = document.getElementById('nextMonth');
+
+let currentDate = new Date();
+
