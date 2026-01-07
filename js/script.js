ecfa30d5d92c00d0a8d07581f8374107d8597ca4
pareesa969 <pareesakhan613@gmail.com>
2026-01-07 17:42:45 +0500

    feat: implement calendar rendering and navigation logic

--- js/script.js
+++ js/script.js
@@ -7,3 +7,55 @@ const nextMonth = document.getElementById('nextMonth');
 
 let currentDate = new Date();
 
+const renderCalendar = () => {
+    calendarDays.innerHTML = '';
+
+    const month = currentDate.getMonth();
+    const year = currentDate.getFullYear();
+
+    const firstDay = new Date(year, month, 1).getDate();
+    const lastDay = new Date(year, month + 1, 0).getDate();
+
+    monthYear.textContent = currentDate.toLocaleString("default", {
+        month: 'long',
+        year: 'numeric'
+    });
+
+    for (let i = 0; i < firstDay; i++){
+        calendarDays.appendChild(document.createElement('div'));
+    }
+
+    for (let day = 1; day <= lastDay; day++) {
+        const dayDiv = document.createElement('div');
+        dayDiv.textContent = day;
+
+        const today = new Date();
+        if(
+            day === today.getDate() &&
+            month === today.getMonth() &&
+            year === today.getFullYear()
+        ) {
+            dayDiv.classList.add('today');
+        }
+        
+        dayDiv.addEventListener('click', () => {
+            const clickedDate = new Date(year, month, day);
+            const dayName = clickedDate.toLocaleString("default", { weekday: 'long' });
+            selectedDate.textContent = `${dayName}, ${day} ${monthYear.textContent}`;
+        });
+        calendarDays.appendChild(dayDiv);
+    }
+
+};
+
+prevMonth.addEventListener('click', () => {
+    currentDate.setMonth(currentDate.getMonth() - 1);
+    renderCalendar();
+});
+
+nextMonth.addEventListener('click', () => {
+    currentDate.setMonth(currentDate.getMonth() + 1);
+    renderCalendar();
+});
+
+renderCalendar();
